e7b0209be82fad97aad63e8324280fbf76b67c6a
peernohell <peernohell@gmail.com>
2014-12-10 09:24:01 +0100

    update tempalte
     * rename tempalte hello to chart
     * click on button add dynamicly data
     * remove separate axes for data1 and data2
     * update c3 to version 1.1.0

--- test-app.js
+++ test-app.js
@@ -1,36 +1,45 @@
 if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault("counter", 0);
 
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get("counter");
-    }
-  });
-
-  Template.hello.events({
+  Template.chart.events({
     'click button': function () {
       // increment the counter when button is clicked
-      Session.set("counter", Session.get("counter") + 1);
+      var list = Session.get('x');
+      list.push(list[list.length - 1] + 20);
+      Session.set('x', list);
+
+      list = Session.get('data1');
+      list.push(Math.random() * 200 | 0);
+      Session.set('data1', list);
+
+      list = Session.get('data2');
+      list.push(Math.random() * 220 | 0);
+      Session.set('data2', list);
     }
   });
 
-  Template.hello.rendered = function () {
+  Template.chart.rendered = function () {
+    Session.set('x', ['x', 30, 50, 75, 100, 120]);
+    Session.set('data1', ['data1', 30, 200, 100, 400, 150]);
+    Session.set('data2', ['data2', 20, 180, 240, 100, 190]);
     var chart = c3.generate({
+	bindto: this.find('.chart'),
         data: {
-            xs: {
-                'data1': 'x1',
-                'data2': 'x2',
-            },
-            columns: [
-                ['x1', 10, 30, 45, 50, 70, 100],
-                ['x2', 30, 50, 75, 100, 120],
-                ['data1', 30, 200, 100, 400, 150, 250],
-                ['data2', 20, 180, 240, 100, 190]
-            ]
+          xs: {
+            'data1': 'x',
+            'data2': 'x'
+          },
+          columns: [['x'],['data1'],['data2']]
         }
     });
-    $('#chart').append(chart.element);
+
+    this.autorun(function (tracker) {
+      chart.load({columns: [
+        Session.get('x'),
+        Session.get('data1'),
+        Session.get('data2'),
+        []
+      ]});
+    });
   }
 }
 
